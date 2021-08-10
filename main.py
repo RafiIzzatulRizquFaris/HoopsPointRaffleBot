5398faeae1f31e286cc133d7a7fb189d0a868b5d
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2021-08-10 14:17:07 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import json
+
+import pandas as pd
+from requests import Session
+
+
+def login_func(email, password, id_card):
+    with Session() as s:
+        login_data = {"username": email, "password": password, "grant_type": "password"}
+        response = s.post("https://www.hoopspoint.com/token", login_data)
+        json_login = json.loads(str(response.content))
+        token = json_login['access_token']
+        user_id = json.loads(str(json_login['user']))['Id']
+        print (user_id)
+        submit_data = {
+            "IdProductOption": 21628,
+            "IdRaffle": 76,
+            "IdUser": user_id,
+            "Identification": id_card,
+            "Options": "<div>Size: 8</div>",
+        }
+        s.post("https://www.hoopspoint.com/api/raffle", submit_data, headers={"authorization": "Bearer " + token})
+
+
+df = pd.read_excel(r'/Users/rafiizzatulrizqufaris/Downloads/testeratmos.xls', sheet_name='Sheet3')
+
+for x in df.values:
+    login_func(x[3], "Hendrajwr121718", x[1])
